3a56390ca779f36fa21ca4d47a6860fb495e78eb
Alexander Ivkin <a@ivkin.dev>
2022-05-04 12:04:30 -0400

    Renamed frame files to jpg

--- utils/cli/png_to_jpg.py
+++ utils/cli/png_to_jpg.py
@@ -16,16 +16,29 @@ def video_to_images(dir):
     for filename in sorted(os.listdir(dir)):
         path = os.path.join(dir, filename)
         if os.path.isfile(path):
-            prefix_match = re.search('(frame_\\d+)\\.PNG', filename, re.IGNORECASE)
+            prefix_match = re.search('frame_(\\d+)\\.PNG', filename, re.IGNORECASE)
             if prefix_match:
                 print("converting " + filename + "...")
-                out_name = prefix_match.group(1)
-                jpg_path = os.path.join(dir, out_name + '.jpg')
+                out_name = '00' + prefix_match.group(1) + '.jpg'
+                jpg_path = os.path.join(dir, out_name)
                 im = Image.open(path)
                 rgb_im = im.convert('RGB')
                 rgb_im.save(jpg_path)
                 os.remove(path)
 
+def update_coco_paths(dir):
+    path = os.path.join(dir, 'annotations', 'instances_default.json')
+    if os.path.isfile(path):
+        with open(path, 'r') as f:
+            text = f.read()
+            text = re.sub('frame_(\\d+)\\.PNG', '00\\1.jpg', text)
+            f.close()
+            with open(path, 'w') as out:
+                out.truncate()
+                out.write(text)
+                out.close()
+
 if __name__ == '__main__':
     dir = sys.argv[1]
     video_to_images(dir)
+    update_coco_paths(dir)
